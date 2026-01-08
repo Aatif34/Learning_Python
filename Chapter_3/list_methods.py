909d7da76074860e6af320f45e46fe5bea82f754
Aatif Mashkoor <mraatif05@gmail.com>
2026-01-08 23:03:30 +0530

    Add list methods example in list_methods.py

---
+++ Chapter_3/list_methods.py
@@ -0,0 +1,10 @@
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+print(friends)
+friends.append("Harry")
+print(friends)
+
+l1 = [1, 34,62, 2, 6, 11]
+
+value = l1.pop(3)
+print(value)
+print(l1)
